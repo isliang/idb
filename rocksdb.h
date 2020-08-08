540d4b019377302ff33f5442f1a8b45e32d24c24
isliang <yesuhuangsi@163.com>
2020-08-08 23:36:55 +0800

    Update rocksdb.h

--- rocksdb.h
+++ rocksdb.h
@@ -2,20 +2,23 @@
 #define IDB_ROCKSDB_H
 #include <rocksdb/db.h>
 
+using namespace ROCKSDB_NAMESPACE;
+using namespace std;
+
 struct ValueData {
     zend_bool is_success;
-    std::string value;
+    string value;
 };
 
 class RocksDB
 {
 private:
-    rocksdb::Status status;
+    Status status;
     zend_bool is_open = false;
-    std::string m_last_error;
+    string m_last_error;
     char *path;
 public:
-    zend_bool open(zend_bool readonly, rocksdb::Options options);
+    zend_bool open(zend_bool readonly, Options options);
     zend_bool put(char *key, char *value);
     ValueData get(char *key);
     zend_string* lastError(void);
